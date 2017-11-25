9c45ed314fa0c23bc969c6393680c4510cda4dc3
Captain.B <bin@fit2cloud.com>
2017-11-25 20:27:43 +0800

    一点点改动

--- routes/nested/app/ts/components/ProductsComponent.ts
+++ routes/nested/app/ts/components/ProductsComponent.ts
@@ -15,27 +15,27 @@ import {
 /*
  * Components
  */
-import {MainComponent} from './products/MainComponent';
-import {InterestComponent} from './products/InterestComponent';
-import {SportifyComponent} from './products/SportifyComponent';
-import {ByIdComponent} from './products/ByIdComponent';
+import { MainComponent } from './products/MainComponent';
+import { InterestComponent } from './products/InterestComponent';
+import { SportifyComponent } from './products/SportifyComponent';
+import { ByIdComponent } from './products/ByIdComponent';
 
 @Component({
   selector: 'products',
   template: `
-  <h2>Products</h2>
+    <h2>Products</h2>
 
-  <div class="navLinks">
-    <a [routerLink]="['./main']">Main</a> |
-    <a [routerLink]="['./interest']">Interest</a> |
-    <a [routerLink]="['./sportify']">Sportify</a> |
-    Enter id: <input #id size="6">
-    <button (click)="goToProduct(id.value)">Go</button>
-  </div>
+    <div class="navLinks">
+      <a [routerLink]="['./main']">Main</a> |
+      <a [routerLink]="['./interest']">Interest</a> |
+      <a [routerLink]="['./sportify']">Sportify</a> |
+      Enter id: <input #id size="6">
+      <button (click)="goToProduct(id.value)">Go</button>
+    </div>
 
-  <div class="products-area">
-    <router-outlet></router-outlet>
-  </div>
+    <div class="products-area">
+      <router-outlet></router-outlet>
+    </div>
   `
 })
 
@@ -43,17 +43,17 @@ export class ProductsComponent {
   constructor(private router: Router, private route: ActivatedRoute) {
   }
 
-  goToProduct(id:string): void {
-    this.router.navigate(['./', id], {relativeTo: this.route});
+  goToProduct(id: string): void {
+    this.router.navigate(['./', id], {relativeTo: this.route});// 相对于那一个route
   }
 }
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'main', pathMatch: 'full' },
-  { path: 'main', component: MainComponent },
-  { path: ':id', component: ByIdComponent },
-  { path: 'interest', component: InterestComponent },
-  { path: 'sportify', component: SportifyComponent },
+  {path: '', redirectTo: 'main', pathMatch: 'full'},
+  {path: 'main', component: MainComponent},
+  {path: ':id', component: ByIdComponent},
+  {path: 'interest', component: InterestComponent},
+  {path: 'sportify', component: SportifyComponent},
 ];
 
 @NgModule({
@@ -71,7 +71,8 @@ export const routes: Routes = [
     SportifyComponent,
     ByIdComponent
   ],
-  imports: [ RouterModule ]
+  imports: [RouterModule]
 })
-export class ProductsComponentModule {}
+export class ProductsComponentModule {
+}
 
